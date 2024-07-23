6e31fc282b617fd68e6941114e1d52b89064ed4e
7week <satwikkumar055@gmail.com>
2024-07-23 13:13:30 +0530

    Initial Commit

---
+++ app/src/main/java/com/ironclad/brute/core/designsystem/components/BruteSearchbar.kt
@@ -0,0 +1,9 @@
+package com.ironclad.brute.core.designsystem.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BruteSearchbar(modifier: Modifier = Modifier) {
+
+}
---
+++ app/src/main/java/com/ironclad/brute/features/home/HomeScreen.kt
@@ -0,0 +1,19 @@
+package com.ironclad.brute.features.home
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.ironclad.brute.core.designsystem.theme.Black
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Black
+    ) {
+
+    }
+
+}
